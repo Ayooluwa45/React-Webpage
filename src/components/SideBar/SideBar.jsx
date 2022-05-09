52ee602e63e5a357d04f0cfc9ace962d29601b13
Emmanuel Sanni <sanniayooluwa45@gmail.com>
2022-05-09 16:08:17 +0200

    SideBar component

---
+++ src/components/SideBar/SideBar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { SideBarContainer, Icon } from './SideBarElements.js'
+
+function SideBar() {
+  return (
+    <>
+      <SideBarContainer>
+        <Icon></Icon>
+      </SideBarContainer>
+    </>
+  );
+}
+
+export default SideBar;
