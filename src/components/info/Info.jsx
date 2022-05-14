6050cd3ab8ae4177f30824db8699c2f71df5a725
Emmanuel Sanni <sanniayooluwa45@gmail.com>
2022-05-15 00:05:20 +0200

    Info Container

---
+++ src/components/info/Info.jsx
@@ -0,0 +1,46 @@
+import {Button} from "react-scroll";
+import {
+    InfoContainer,
+    InfoWrapper,
+    InfoRow,
+    Column1,
+    Column2,
+    TextWrapper,
+    TopLine,
+    Heading,
+    Subtitle,
+    BtnWrap,
+    InfoWrap,
+    Img
+} from './InfoElement'
+
+function Info() {
+  return (
+    <>
+      <InfoContainer>
+        <InfoWrapper>
+          <InfoRow>
+            <Column1>
+              <TextWrapper>
+                <TopLine>TopLine</TopLine>
+                <Heading>Heading</Heading>
+                <Subtitle>Subtitle</Subtitle>
+                <BtnWrap>
+                  <Button to="home">Button</Button>
+                </BtnWrap>
+              </TextWrapper>
+            </Column1>
+            <Column2>
+              <ImgWrap>
+                <Img />
+              </ImgWrap>
+            </Column2>
+          </InfoRow>
+        </InfoWrapper>
+        <InfoRow></InfoRow>
+      </InfoContainer>
+    </>
+  );
+}
+
+export default Info;
