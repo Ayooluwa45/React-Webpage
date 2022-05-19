537408f745848b7b71ef1ac4cdd870860e47ab66
Emmanuel Sanni <sanniayooluwa45@gmail.com>
2022-05-19 13:27:49 +0200

    Footer components and styles

---
+++ src/components/footer/FooterStyle.js
@@ -0,0 +1,54 @@
+import React from "react";
+
+function FooterStyle() {
+  return (
+    <FooterContainer>
+      <FooterWrap>
+        <FooterLinksContainer>
+          <FooterLinksWrapper>
+            <FooterLinkItems>
+              <FooterLinkTitle>About us</FooterLinkTitle>
+
+              <FooterLink to="/signin">How it works</FooterLink>
+              <FooterLink to="/signin">Testimonial</FooterLink>
+              <FooterLink to="/signin">Career</FooterLink>
+              <FooterLink to="/signin">Investors</FooterLink>
+              <FooterLink to="/signin">Term of service</FooterLink>
+            </FooterLinkItems>
+            <FooterLinkItems>
+              <FooterLinkTitle>About us</FooterLinkTitle>
+
+              <FooterLink to="/signin">How it works</FooterLink>
+              <FooterLink to="/signin">Testimonial</FooterLink>
+              <FooterLink to="/signin">Career</FooterLink>
+              <FooterLink to="/signin">Investors</FooterLink>
+              <FooterLink to="/signin">Term of service</FooterLink>
+            </FooterLinkItems>
+          </FooterLinksWrapper>
+          <FooterLinksWrapper>
+            <FooterLinkItems>
+              <FooterLinkTitle>About us</FooterLinkTitle>
+
+              <FooterLink to="/signin">How it works</FooterLink>
+              <FooterLink to="/signin">Testimonial</FooterLink>
+              <FooterLink to="/signin">Career</FooterLink>
+              <FooterLink to="/signin">Investors</FooterLink>
+              <FooterLink to="/signin">Term of service</FooterLink>
+            </FooterLinkItems>
+            <FooterLinkItems>
+              <FooterLinkTitle>About us</FooterLinkTitle>
+
+              <FooterLink to="/signin">How it works</FooterLink>
+              <FooterLink to="/signin">Testimonial</FooterLink>
+              <FooterLink to="/signin">Career</FooterLink>
+              <FooterLink to="/signin">Investors</FooterLink>
+              <FooterLink to="/signin">Term of service</FooterLink>
+            </FooterLinkItems>
+          </FooterLinksWrapper> 
+        </FooterLinksContainer>
+      </FooterWrap>
+    </FooterContainer>
+  );
+}
+
+export default FooterStyle;
